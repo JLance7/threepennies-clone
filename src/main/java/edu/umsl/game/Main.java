fb3b33723127ba286b0238b683eb29c224e40dc7
asjbw4 <asjbw4@umsl.edu>
2022-04-27 10:26:18 -0500

    added further credit for assets

--- src/main/java/edu/umsl/game/Main.java
+++ src/main/java/edu/umsl/game/Main.java
@@ -23,15 +23,33 @@ Source 1 - This is an expansive tutorial on Swing. It was instrumental in the de
 that is used in this program. The knowledge provided by this source was utilized while developing
 the entirety of the front-end of this program.
 <https://youtu.be/Kmgo00avvEw>
+
+Source 2 - This video was the inspiration for the project and gave us many of our ideas for the game.
+<https://youtu.be/Sa9jLWKrX0c>
+
+Asset Credit:
+The background music was provided by the Royalty Free Musician, Kevin Mcleod. The name of the song
+used is "Local Forecast - Elevator" by Kevin Mcleod. The links for the song and his website follow.
+<https://youtu.be/FgXYzF5-Yiw>
+<https://incompetech.com/music/royalty-free/>
+
+"Nice" Sound Effect was created by Michael Rosen. All credit for the sound goes to said named
+individual.
+
+"Bruh" Sound Effect was created by the vine user, Headgraphix. All credit for the sound goes
+to said named individual.
+
+Both "Coin Flip" and "Button Click" sound effects were provided by FreeSound.org under the Creative
+Commons 0 license.
+
+Multiple "Among Us" images are used in this creation for purely educational and nondistributive
+purposes. "Among Us" © was created Innersloth LLC. The Unhandles Exception Team claims no ownership
+over the "Among Us" IP.
 ----------------------------------------------------------------------------------------------------------*/
 
 package edu.umsl.game;
 
 //import for access to GameFrame
-
-//bruh and nice sounds taken from youtube
-//buttonClick and coinFlip sounds taken from Freesound.org
-//Elevator by Kevin Macleod song used
 import edu.umsl.game.gui.frame.GameFrame;
 
 import javax.swing.*;
